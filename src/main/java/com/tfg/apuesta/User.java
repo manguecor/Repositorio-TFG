7118d3a4de711dc28a5aac4641b0b0558a1f0029
manlopalm <manlopalm@alum.us.es>
2022-02-15 19:08:33 +0100

    Inicio de página web

---
+++ src/main/java/com/tfg/apuesta/User.java
@@ -0,0 +1,55 @@
+package com.tfg.apuesta;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+ 
+    private static final long serialVersionUID = 1L;
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+     
+    @Column(name = "name")
+    private String name;
+ 
+    @Column(name = "username")
+    private String username;
+ 
+ 
+    public User() {
+        super();
+    }
+     
+    public User(Long id, String name, String username) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.username = username;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
