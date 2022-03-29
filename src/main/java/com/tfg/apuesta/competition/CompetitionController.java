b6f73c2b3bc48ab85f09fc01b7ba55fb548768b9
manguecor <manguecor@alum.us.es>
2022-03-29 13:50:37 +0200

    Feat#22: Añadido rest al controlador

--- src/main/java/com/tfg/apuesta/competition/CompetitionController.java
+++ src/main/java/com/tfg/apuesta/competition/CompetitionController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@Controller
+@RestController
 public class CompetitionController {
 	
 	private final CompetitionService competitionService;
