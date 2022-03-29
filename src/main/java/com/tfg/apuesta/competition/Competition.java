4a3abbb6bf102c7ebae7a0dde9622a4075931e5b
manguecor <manguecor@alum.us.es>
2022-03-29 13:38:26 +0200

    Feat#22: Consultas de la API

--- src/main/java/com/tfg/apuesta/competition/Competition.java
+++ src/main/java/com/tfg/apuesta/competition/Competition.java
@@ -1,19 +1,10 @@
 package com.tfg.apuesta.competition;
 
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-import com.tfg.apuesta.bet.Bet;
 import com.tfg.apuesta.configuration.BaseEntity;
-import com.tfg.apuesta.match.Match;
-import com.tfg.apuesta.team.Team;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -27,25 +18,37 @@ public class Competition extends BaseEntity {
 	@NotNull
 	private String name;
 	
-	@NotNull
-	private String clasification;
+	private String emblemUrl;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmblemUrl() {
+		return emblemUrl;
+	}
+
+	public void setEmblemUrl(String emblemUrl) {
+		this.emblemUrl = emblemUrl;
+	}
+
+	public Competition(@NotNull String name,String emblemUrl) {
+		super();
+		this.name = name;
+		this.emblemUrl = emblemUrl;
+	}
 	
-	@NotNull
-	private String scorers;
+	public Competition() {
+		super();
+	}
 	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "competition")
+	/*@OneToMany(cascade = CascadeType.ALL, mappedBy = "competition")
     private Set<Match> matches;
 	
 	@ManyToMany(mappedBy="competitions")
-    private Set<Team> teams;
-
-	@Override
-	public String toString() {
-		return "Competition [name=" + name + ", clasification=" + clasification + ", scorers=" + scorers + "]";
-	}
-	
-	
-	
-	
-
+    private Set<Team> teams;*/	
 }
--- src/main/java/com/tfg/apuesta/competition/CompetitionController.java
+++ src/main/java/com/tfg/apuesta/competition/CompetitionController.java
@@ -1,12 +1,13 @@
 package com.tfg.apuesta.competition;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CompetitionController {
@@ -23,7 +24,7 @@ public class CompetitionController {
 		dataBinder.setDisallowedFields("id");
 	}
 	
-	@GetMapping("/competition/{competitionId}")
+	/*@GetMapping("/competition/{competitionId}")
 	public ModelAndView showCompetition(@PathVariable("competitionId") int competitionId) {
 		ModelAndView mav = new ModelAndView("/welcome");
 		mav.addObject(this.competitionService.findCompetitionById(competitionId));
@@ -35,6 +36,21 @@ public class CompetitionController {
 		ModelAndView mav = new ModelAndView("/welcome");
 		mav.addObject(this.competitionService.findAllCompetitions());
 		return mav;
+	}*/
+	
+	@GetMapping("/competitions")
+	public List<Competition> getCompetitions() {
+		return this.competitionService.showAllCompetitions();
+	}
+	
+	@GetMapping("/competition/{competitionId}")
+	public List<String> getClasification(@PathVariable("competitionId") int competitionId) {
+		return this.competitionService.showClasificationByCompetition(competitionId);
+	}
+	
+	@GetMapping("/scorers/{competitionId}")
+	public List<String> getScorers(@PathVariable("competitionId") int competitionId) {
+		return this.competitionService.showScorersByCompetition(competitionId);
 	}
 
 }
