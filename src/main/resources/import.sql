a2a6bf181d99bb05cd027630318bdfa9098291ed
manguecor <manguecor@alum.us.es>
2022-05-17 23:38:43 +0200

    Feat#55: Cambios en el listado de apuestas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,7 +14,12 @@ INSERT INTO bets(bet_type,description,estado,league_id,player_id) VALUES (0,'Apu
 INSERT INTO matches(api_id,away_team,competition,home_team,match_date,result,status,bet_id) VALUES (325784,'SC Heerenveen','Eredivisie','Go Ahead Eagles',null,'AWAY_TEAM','FINISHED',1);
 INSERT INTO player_bet(match_id,player_result,bet_id,player_id) VALUES (325784,'DRAW',1,1);
 INSERT INTO matches(api_id,away_team,competition,home_team,match_date,result,status,bet_id) VALUES (325783,'AZ','Eredivisie','RKC Waalwijk',null,'HOME_TEAM','FINISHED',1);
-INSERT INTO player_bet(match_id,player_result,bet_id,player_id) VALUES (325783,'AWAY_TEAM',1,1);
+INSERT INTO player_bet(match_id,player_result,bet_id,player_id) VALUES (325783,'HOME_TEAM',1,1);
+
 INSERT INTO bets(bet_type,description,estado,league_id,player_id) VALUES (1,'Eredivisie','PENDIENTE',1,1);
+INSERT INTO matches(api_id,away_team,competition,home_team,match_date,result,status,bet_id) VALUES (325784,'SC Heerenveen','Eredivisie','Go Ahead Eagles',null,'0-1','FINISHED',2);
 INSERT INTO player_bet(match_id,player_result,bet_id,player_id) VALUES (325784,'0-1',2,1);
+INSERT INTO matches(api_id,away_team,competition,home_team,match_date,result,status,bet_id) VALUES (325783,'AZ','Eredivisie','RKC Waalwijk',null,'1-0','FINISHED',2);
 INSERT INTO player_bet(match_id,player_result,bet_id,player_id) VALUES (325783,'1-1',2,1);
+INSERT INTO matches(api_id,away_team,competition,home_team,match_date,result,status,bet_id) VALUES (325782,'PSV','Eredivisie','Heracles Almelo',null,'0-2','FINISHED',2);
+INSERT INTO player_bet(match_id,player_result,bet_id,player_id) VALUES (325782,'3-3',2,1);
